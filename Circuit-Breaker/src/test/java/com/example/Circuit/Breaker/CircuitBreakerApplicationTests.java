d96ab76f18519c3f213c26864370911e13dd7615
kavyasri123345 <pochanakavyasri0206@gmail.com>
2025-02-28 17:54:43 +0530

    commit

---
+++ Circuit-Breaker/src/test/java/com/example/Circuit/Breaker/CircuitBreakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Circuit.Breaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircuitBreakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
